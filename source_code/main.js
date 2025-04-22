142b139c478735a7f30ec777df2d6306925b7615
For_est0512 <jerry@17mall.com.tw>
2025-04-22 22:43:25 +0800

    discord_bot_setup_20250422

---
+++ source_code/main.js
@@ -0,0 +1,14 @@
+// import
+import {Client,Events,GatewayIntentBits} from 'discord.js'
+import dotenv from 'dotenv' //存取環境變數工具
+
+dotenv.config()//調用環境變數工具
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.token);
